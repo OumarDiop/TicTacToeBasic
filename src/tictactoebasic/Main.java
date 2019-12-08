237f28457637841ace91403439dda2b577aa19ac
Oumar DIop <oumardiop2003@gmail.com>
2019-12-08 14:46:09 -0800

    Finished basic game

--- src/tictactoebasic/Main.java
+++ src/tictactoebasic/Main.java
@@ -1,17 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tictactoebasic;
 
-/**
- *
- * @author coolDawg1234
- */
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner oumar = new Scanner(System.in);
+        TTT game = new TTT();
+        game.initializeBoard();
+        System.out.println("Tic-Tac-Toe");
+        do {
+            System.out.println("Board:");
+            game.printBoard();
+            int row;
+            int col;
+            do {
+                System.out.println("Player " + game.getCurrentPlayerMark() + " type in valid row and column:");
+                row = oumar.nextInt()-1;
+                col = oumar.nextInt()-1;
+            }
+            while (!game.placeMark(row, col));
+            game.changePlayer();
+        }   
+        while (!game.checkForWin() && !game.isBoardFull());
+        if (game.isBoardFull() && !game.checkForWin()) {
+            System.out.println("Tie Game.");
+        } 
+        else {
+            System.out.println("Board:");
+            game.printBoard();
+            game.changePlayer();
+            System.out.println(Character.toUpperCase(game.getCurrentPlayerMark()) + " won");
+        }
     }
 }
